61cad876058aa424d2b4912e170c6beeb76acd8c
ColinJr <colindetwillerjr@gmail.com>
2021-03-17 15:21:28 -0400

    FizzBuzz 2, string version

---
+++ 031721KataFizzBuzz/031721KataFizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _031721KataFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++){
+                string fizzBuzz = "";
+
+                if (i % 3 == 0) {
+                    fizzBuzz += "Fizz";
+                };
+                if (i % 5 == 0) {
+                    fizzBuzz += "Buzz";
+                };
+                if (fizzBuzz == ""){
+                    fizzBuzz += i;
+                };
+                Console.WriteLine(fizzBuzz);
+            }
+        }
+    }
+}
