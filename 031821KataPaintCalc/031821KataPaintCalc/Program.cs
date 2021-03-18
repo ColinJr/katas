210a245b4527a66d640ba7a243f839521e107e23
ColinJr <colindetwillerjr@gmail.com>
2021-03-18 12:50:25 -0400

    Finished Ceiling Paint Calculator Kata

--- 031821KataPaintCalc/031821KataPaintCalc/Program.cs
+++ 031821KataPaintCalc/031821KataPaintCalc/Program.cs
@@ -6,7 +6,26 @@ namespace _031821KataPaintCalc
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Let's find how much paint is needed for your ceiling!");
+
+            Console.WriteLine("How many square feet can each bucket of your chosen paint cover?");
+            float areaPerCan = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("What is your room's length in feet?");
+            float roomLength = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your room's width in feet?");
+            float roomWidth = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("What is your room's height?");
+            //int roomHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("So your room is " + roomLength + " feet by " + roomWidth + " feet?");
+            float ceilingArea = roomLength*roomWidth;
+            float numOfCans = (int)Math.Ceiling(ceilingArea/areaPerCan);
+            Console.WriteLine("Your ceiling is " + ceilingArea + " square feet. And needs " + numOfCans + " buckets of paint");
         }
     }
 }
