6e947d2df91c137a5e6b05b7dfd6c359172b6b40
ColinJr <colindetwillerjr@gmail.com>
2021-03-18 16:11:08 -0400

    Kata 2.0 Before I forget

--- 031821KataPaintCalc/031821KataPaintCalc/Program.cs
+++ 031821KataPaintCalc/031821KataPaintCalc/Program.cs
@@ -6,26 +6,74 @@ namespace _031821KataPaintCalc
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Let's find how much paint is needed for your room!");
 
-            Console.WriteLine("Let's find how much paint is needed for your ceiling!");
+            float areaPerCan;
+            float roomLength;
+            float roomWidth;
+            float roomHeight;
+            int numOfObstructions;
+            float totalObstructionArea = 0;
+           
+            
+            string correct;
 
-            Console.WriteLine("How many square feet can each bucket of your chosen paint cover?");
-            float areaPerCan = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("How many square feet can each bucket of your chosen paint cover?");
+                areaPerCan = (float) Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("What is your room's length in feet?");
+                roomLength = (float) Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("What is your room's width in feet?");
+                roomWidth = (float) Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("What is your room's height?");
+                roomHeight = (float) Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("So your room is " + roomLength + " feet long, " + roomWidth + " feet wide, and " + roomHeight + " feet tall?");
+                Console.WriteLine("(yes/no)");
+                correct = Console.ReadLine().ToLower();
+                
+            }
+            while (correct != "yes");
 
 
-            Console.WriteLine("What is your room's length in feet?");
-            float roomLength = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("How many doorways, windows or other paint free sections are there?");
+                numOfObstructions = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("What is your room's width in feet?");
-            float roomWidth = Convert.ToInt32(Console.ReadLine());
+                for (int currentObstruction = 1; currentObstruction <= numOfObstructions; currentObstruction++)
+                {
+                    Console.WriteLine("Door/Window #" + currentObstruction);
+                    Console.WriteLine("What is the length of the " + currentObstruction + "th door/window, in feet?");
+                    float obstructionLength = (float) Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("What is the height of the " + currentObstruction + "th door/window, in feet?");
+                    float obstructionHeight = (float) Convert.ToDouble(Console.ReadLine());
+                    float obstructionArea = obstructionHeight * obstructionLength;
+                    totalObstructionArea = totalObstructionArea + obstructionArea;
+                }
+                Console.WriteLine("Was all that Correct? (yes/no)");
+                correct = Console.ReadLine().ToLower();
+            }
+            while (correct != "yes");
 
-            //Console.WriteLine("What is your room's height?");
-            //int roomHeight = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("So your room is " + roomLength + " feet by " + roomWidth + " feet?");
-            float ceilingArea = roomLength*roomWidth;
-            float numOfCans = (int)Math.Ceiling(ceilingArea/areaPerCan);
-            Console.WriteLine("Your ceiling is " + ceilingArea + " square feet. And needs " + numOfCans + " buckets of paint");
+            float ceilingArea = roomLength * roomWidth;
+            float walls13Area = 2 * roomLength * roomHeight;
+            float walls24Area = 2 * roomWidth * roomHeight;
+            float totalPaintableArea = ceilingArea + walls13Area + walls24Area;
+            Console.WriteLine(totalPaintableArea + " is the total area of all the walls and ceiling");
+
+
+
+            Console.WriteLine(totalObstructionArea + " is the total area we don't need to paint, due to doors/windows");
+            totalPaintableArea = totalPaintableArea - totalObstructionArea;
+            float numOfCans = (int)Math.Ceiling(totalPaintableArea/areaPerCan);
+            Console.WriteLine("Your room has " + totalPaintableArea + " square feet of surface in need of paint. And painting it will take " + numOfCans + " cans of paint");
         }
     }
+       
 }
